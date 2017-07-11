c9211fe6acb7fc72b4f0419251176bc273cd91e1
Matjaz Munda <matjaz@saltoplus.si>
2017-07-11 15:15:52 +0200

    - removed unnecessary listeners

--- wear/src/main/java/si/saltoplus/testmsgonwear/MsgListenerService.java
+++ wear/src/main/java/si/saltoplus/testmsgonwear/MsgListenerService.java
@@ -3,14 +3,8 @@ package si.saltoplus.testmsgonwear;
 import android.content.Intent;
 import android.util.Log;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.Wearable;
 import com.google.android.gms.wearable.WearableListenerService;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by matjazmunda on 11/07/2017.
@@ -24,18 +18,18 @@ public class MsgListenerService extends WearableListenerService {
     public static final String MESSAGE_PATH = "/msg";
     public static final String ACTIVITY_PATH = "/startActivity";
 
-    GoogleApiClient mGoogleApiClient;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        LOGD(TAG, "Service created");
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .build();
-        mGoogleApiClient.connect();
-    }
+//    GoogleApiClient mGoogleApiClient;
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        LOGD(TAG, "Service created");
+//
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addApi(Wearable.API)
+//                .build();
+//        mGoogleApiClient.connect();
+//    }
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
@@ -50,20 +44,20 @@ public class MsgListenerService extends WearableListenerService {
         LOGD(TAG, "onMessageReceived: finished");
     }
 
-    @Override
-    public void onDataChanged(DataEventBuffer dataEvents) {
-        LOGD(TAG, "onDataChanged: " + dataEvents);
-        if (!mGoogleApiClient.isConnected() || !mGoogleApiClient.isConnecting()) {
-            ConnectionResult connectionResult = mGoogleApiClient
-                    .blockingConnect(30, TimeUnit.SECONDS);
-            if (!connectionResult.isSuccess()) {
-                Log.e(TAG, "DataLayerListenerService failed to connect to GoogleApiClient, "
-                        + "error code: " + connectionResult.getErrorCode());
-                return;
-            }
-        }
-        LOGD(TAG, "onDataChanged: finished");
-    }
+//    @Override
+//    public void onDataChanged(DataEventBuffer dataEvents) {
+//        LOGD(TAG, "onDataChanged: " + dataEvents);
+//        if (!mGoogleApiClient.isConnected() || !mGoogleApiClient.isConnecting()) {
+//            ConnectionResult connectionResult = mGoogleApiClient
+//                    .blockingConnect(30, TimeUnit.SECONDS);
+//            if (!connectionResult.isSuccess()) {
+//                Log.e(TAG, "DataLayerListenerService failed to connect to GoogleApiClient, "
+//                        + "error code: " + connectionResult.getErrorCode());
+//                return;
+//            }
+//        }
+//        LOGD(TAG, "onDataChanged: finished");
+//    }
 
     public static void LOGD(final String tag, String message) {
         if (Log.isLoggable(tag, Log.DEBUG)) {
